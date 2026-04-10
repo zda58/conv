e01ceabdf53a8609d3e391ab5f87dada50670258
zda58 <107224274+zda58@users.noreply.github.com>
2026-04-09 20:18:25 -0400

    add imp files

--- imp.py
+++ imp.py
@@ -2,4 +2,4 @@ import pandas as pd
 from pathlib import Path
 
 for f in Path('./clean').glob('*.csv'):
-    pd.read_csv(f).to_csv('./imp' + f, index=False, na_rep=r'\N')
+    pd.read_csv(f).to_csv('./imp/' + f.name, index=False, na_rep=r'\N')
